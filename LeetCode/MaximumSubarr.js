f03c4f3f8df7d5e1a633ef98ab7c882035d13729
Sneha Thadani <snehacopper@gmail.com>
2019-10-21 22:05:07 -0700

    maximum SubArray(53)

--- LeetCode/MaximumSubarr.js
+++ LeetCode/MaximumSubarr.js
@@ -8,7 +8,7 @@ Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
 
 */
-var maxSubArray = function(nums) {
+/*var maxSubArray = function(nums) {
     let maxSum= nums[0];
     let maxsoFar= maxSum;
     
@@ -20,5 +20,29 @@ var maxSubArray = function(nums) {
 
 };
 
+let nums= [1];
+maxSubArray(nums);*/
+let maxSubArray = (nums)=> {
+    if  (nums.length===0) {
+        return 0;
+    } else {
+        return findMaxSum(nums, 0, nums.length-1);
+    }
+    }
+
+let findMaxSum= (nums, left,right)=> {
+    if (left===right){
+        return right;
+    }
+    mid = Math.floor((left + right) / 2);
+    let sum =0;
+    for (i=0; i<nums.length; i++) {
+        leftmax= Math.max(findMaxSum(nums,0,mid), nums[i]);
+        rightmax= Math.max(findMaxSum(nums,mid+1, right),nums[i]);
+        centerMax= Math.max(findMaxSum(nums, 0 + mid/2, mid + right/2))
+    }  
+    return Math.max(leftmax, rightmax, centerMax);
+
+}
 let nums= [1];
 maxSubArray(nums);
